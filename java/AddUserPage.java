cd32faafa64da102da32aab670200b7fe6018264
Saravanan P <vanansaransecondary@gmail.com>
2025-05-25 20:24:51 +0530

    Add code fileAddUserPage.java

--- java/AddUserPage.java
+++ java/AddUserPage.java
@@ -2,43 +2,48 @@ package com.salesforce.pages;
 
 import com.framework.selenium.api.design.Locators;
 import com.framework.testng.api.base.ProjectSpecificMethods;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
-/**
- * Page object class for Add User page.
- */
 public class AddUserPage extends ProjectSpecificMethods {
 
-    // Method to verify the header
-    /**
-     * Verify the header of the Add User page.
-     */
-    public void verifyHeader() {
-        // Locate the header element
-        WebElement header = getDriver().findElement(Locators.XPATH, "//h1");
-        // Verify the header text
-        reportStep("Verifying the header on Add User page", "INFO");
-        if (header.getText().equals("Add User")) {
-            reportStep("Header is verified successfully on Add User page", "PASS");
-        } else {
-            reportStep("Header is not verified on Add User page", "FAIL");
-        }
+    // Method to enter first name
+    public void enterFirstName(String firstName) {
+        // reportStep("Entering first name on Add User page");
+        driver.findElement(Locators.ID, "firstName").sendKeys(firstName);
+        reportStep("Entered first name as " + firstName + " on Add User page");
     }
 
-    // Method to verify the header class
-    /**
-     * Verify the class of the header element.
-     */
-    public void verifyHeaderClass() {
-        // Locate the header element
-        WebElement header = getDriver().findElement(Locators.XPATH, "//h1");
-        // Verify the header class
-        reportStep("Verifying the header class on Add User page", "INFO");
-        if (header.getAttribute("class").equals("genai-highlight genai-selected")) {
-            reportStep("Header class is verified successfully on Add User page", "PASS");
-        } else {
-            reportStep("Header class is not verified on Add User page", "FAIL");
-        }
+    // Method to enter last name
+    public void enterLastName(String lastName) {
+        // reportStep("Entering last name on Add User page");
+        driver.findElement(Locators.ID, "lastName").sendKeys(lastName);
+        reportStep("Entered last name as " + lastName + " on Add User page");
+    }
+
+    // Method to enter email
+    public void enterEmail(String email) {
+        // reportStep("Entering email on Add User page");
+        driver.findElement(Locators.ID, "email").sendKeys(email);
+        reportStep("Entered email as " + email + " on Add User page");
+    }
+
+    // Method to enter password
+    public void enterPassword(String password) {
+        // reportStep("Entering password on Add User page");
+        driver.findElement(Locators.ID, "password").sendKeys(password);
+        reportStep("Entered password on Add User page");
+    }
+
+    // Method to click submit button
+    public void clickSubmitButton() {
+        // reportStep("Clicking submit button on Add User page");
+        driver.findElement(Locators.ID, "submit").click();
+        reportStep("Clicked submit button on Add User page");
+    }
+
+    // Method to click cancel button
+    public void clickCancelButton() {
+        // reportStep("Clicking cancel button on Add User page");
+        driver.findElement(Locators.ID, "cancel").click();
+        reportStep("Clicked cancel button on Add User page");
     }
 }
